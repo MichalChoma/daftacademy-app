55233fe628929c9f62b92c6f80983ccd7f0ae0fd
MichalChoma <macawelly200@gmail.com>
2022-12-15 23:44:10 +0100

    update progressBar

--- src/components/Player/components/ProgressBar/ProgressBar.component.tsx
+++ src/components/Player/components/ProgressBar/ProgressBar.component.tsx
@@ -1,22 +1,18 @@
-import React, { useRef, useState, MouseEvent, useEffect } from "react";
-import styles from "./ProgressBar.module.css";
-import Draggable from "react-draggable";
+import React, { useState, useEffect } from "react";
 import usePlayer from "~/hooks/usePlayer.hook";
+import styles from "./ProgressBar.module.css";
 
 const ProgressBar = () => {
   const { state, actions } = usePlayer();
-  const [progress, setProgress] = useState(state.progress);
-  const [currentTime, setCurrentTime] = useState(state.currentTime);
-  const [wasPlaying, setWasPlaying] = useState(false);
-  const [drag, setDrag] = useState(false);
+  const [position, setPosition] = useState(0);
 
-  const rootRef = useRef<HTMLDivElement | null>(null);
-  const nodeRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    setPosition(state.progress);
+  }, [state.progress]);
 
-  const getParsedTime = (totalSeconds: number) => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-
+  const getParsedTime = (totalSec: number) => {
+    const minutes = Math.floor(totalSec / 60);
+    const seconds = totalSec % 60;
     return (
       <span>
         {minutes < 10 ? `0${minutes}` : minutes}:
@@ -25,90 +21,31 @@ const ProgressBar = () => {
     );
   };
 
-  const getIndicatorXPosition = (percentOffset: number): number => {
-    if (rootRef.current) {
-      return rootRef.current?.offsetWidth * percentOffset;
-    }
-
-    return 0;
-  };
-
-  const getNewProgress = (dragXPosition: number): number => {
-    if (rootRef.current) {
-      return dragXPosition / rootRef.current?.offsetWidth;
-    }
-
-    return 0;
-  };
-
-  const onIndicatorDragStart = () => {
-    setDrag(true);
-
-    setWasPlaying(state.playing);
-    actions?.pause();
-  };
-
-  const onIndicatorDragStop = (dragXPosition: number) => {
-    const newProgress = getNewProgress(dragXPosition);
-    actions.seek(state.duration * newProgress);
-
-    setTimeout(() => setDrag(false), 50);
-    if (wasPlaying) {
-      actions?.play();
-    }
-  };
-
-  const onIndicatorDrag = (dragXPosition: number) => {
-    const newProgress = getNewProgress(dragXPosition);
-
-    setCurrentTime(Math.floor(state.duration * newProgress));
-    setProgress(newProgress);
-  };
-
-  const onBarClick = (e: MouseEvent<HTMLDivElement>) => {
-    if (rootRef.current && !drag) {
-      const target = e.target as HTMLElement;
-      const newProgress =
-        (e.clientX - target.getBoundingClientRect().left) /
-        rootRef.current?.offsetWidth;
-
-      actions.seek(state.duration * newProgress);
-    }
-  };
-
-  useEffect(() => {
-    setProgress(state.progress);
-  }, [state.progress]);
-
-  useEffect(() => {
-    setCurrentTime(state.currentTime);
-  }, [state.currentTime]);
-
   return (
     <div className={styles.root}>
-      <div
-        className={styles.progressContainer}
-        ref={rootRef}
-        onClick={(e) => onBarClick(e)}
-      >
+      <div className={styles.container}>
         <div
-          className={styles.progress}
-          style={{ width: `${progress * 100}%` }}
+          className={styles.progressBar}
+          style={{ width: `${position * 100}%` }}
+        ></div>
+        <div
+          className={styles.thumb}
+          style={{ left: `${position * 100}%`}}
+        ></div>
+        <input
+          type="range"
+          step={0.01}
+          max={state.duration}
+          className={styles.range}
+          onChange={(e) => actions.seek(+e.target.value)}
+          onMouseDown={() => actions.pause()}
+          onTouchStart={() => actions.pause()}
+          onMouseUp={() => actions.play()}
+          onTouchEnd={() => actions.play()}
         />
-        <Draggable
-          nodeRef={nodeRef}
-          bounds="parent"
-          axis="x"
-          position={{ x: getIndicatorXPosition(progress), y: -6 }}
-          onStart={() => onIndicatorDragStart()}
-          onStop={(e, data) => onIndicatorDragStop(data.x)}
-          onDrag={(e, data) => onIndicatorDrag(data.x)}
-        >
-          <div className={styles.indicator} ref={nodeRef} />
-        </Draggable>
       </div>
       <div className={styles.timer}>
-        {getParsedTime(currentTime)} / {getParsedTime(state.duration)}
+        {getParsedTime(state.currentTime)} / {getParsedTime(state.duration)}
       </div>
     </div>
   );
